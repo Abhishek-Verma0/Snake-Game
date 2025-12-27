e0d6fff3eddb92274cedee3a3bfc79ec4e2e9407
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-12-28 05:20:25 +0530

    static positioning done

--- snake.c
+++ snake.c
@@ -3,18 +3,25 @@
 
 #define WIDTH 900
 #define HEIGHT 600
-#define CELL_SIZE 15  // simple size of single cell of grid, a squared cell
+#define CELL_SIZE 30  // simple size of single cell of grid, a squared cell
 #define ROWS HEIGHT/CELL_SIZE   // getting number of rows for grid
 #define COLUMNS WIDTH/CELL_SIZE // getting number of cols for grid
-#define GRID_COLOR 0x1f1f1f1f // grid color
+#define COLOR_GRID 0x1f1f1f1f // grid color
+#define COLOR_WHITE 0xffffffff // Snake color
+#define COLOR_APPLE 0xFFFF0000 // fruit color
 #define LINE_WIDTH 2
+
+#define SNAKE(x,y) fill_cell(surface,x,y,COLOR_WHITE)  // for snake
+#define DRAW_GRID draw_grid(surface) // for the grid
+
+#define APPLE(x,y) fill_cell(surface,x,y,COLOR_APPLE)
 //  funciton for the drawing grid
 int draw_grid(SDL_Surface *surface){
     //  making horizontal line for the gird 
     SDL_Rect line = {0, 0, WIDTH, LINE_WIDTH};
     for (line.y = 0; line.y < HEIGHT;line.y+=CELL_SIZE){ // line is moved down at every step by cell size 
         //  each iteration draw one horizontal line
-        SDL_FillSurfaceRect(surface, &line, GRID_COLOR);
+        SDL_FillSurfaceRect(surface, &line, COLOR_GRID);
     }
 
     //  making vertical line for the grid
@@ -24,8 +31,15 @@ int draw_grid(SDL_Surface *surface){
 
     for (line.x = 0; line.x < WIDTH;line.x+=CELL_SIZE)
     {
-        SDL_FillSurfaceRect(surface, &line, GRID_COLOR);
+        SDL_FillSurfaceRect(surface, &line, COLOR_GRID);
     }
+}
+
+
+//  for snake 
+void fill_cell(SDL_Surface* surface, int x,int y,Uint32 color){
+    SDL_Rect rect = {x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+    SDL_FillSurfaceRect(surface, &rect, color);
 }
 
 int main(){
@@ -35,12 +49,43 @@ int main(){
     SDL_Window* window= SDL_CreateWindow("Classic Snake", WIDTH, HEIGHT,0); // creating an window which opens in center of screen
 
     SDL_Surface *surface = SDL_GetWindowSurface(window); //drawing on window
-    SDL_Rect rect = {200, 200, 200, 200};
-    // SDL_FillRect(surface, &rect, 0xffffffff); // this was used in sdl2 now it has become FillSurfaceRect in sdl3
-    SDL_FillSurfaceRect(surface, &rect, 0xffffffff);
 
-    draw_grid(surface);
-    SDL_UpdateWindowSurface(window);
+    SDL_Event event;
+    //  writing game loop
+    int game = 1;
+    int snake_x = 5;
+    int snake_y = 5;
+    int apple_x = 10;
+    int apple_y = 14;
+    while(game){
 
-    SDL_Delay(5000);
+        while(SDL_PollEvent(&event)){
+            if(event.type== SDL_EVENT_QUIT){
+                game = 0;
+            }
+            if(event.type==SDL_EVENT_KEY_DOWN){
+                if(event.key.key==SDLK_RIGHT){
+                    snake_x++;
+                }
+                if(event.key.key==SDLK_LEFT){
+                    snake_x--;
+                }
+                if(event.key.key==SDLK_UP){
+                    snake_y--;
+                }
+                if(event.key.key==SDLK_DOWN){
+                    snake_y++;
+                }
+            }
+        }
+        
+        
+       
+        SNAKE( snake_x, snake_y);
+        APPLE(apple_x, apple_y);
+        DRAW_GRID;
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(20);
+    }
+
 }

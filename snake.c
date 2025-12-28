49eb07d502479cb5346602a7c70d9e9464d3bc7c
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2025-12-29 03:14:56 +0530

    movement tracking using Data structure

--- snake.c
+++ snake.c
@@ -11,17 +11,29 @@
 #define COLOR_APPLE 0xFFFF0000 // fruit color
 #define LINE_WIDTH 2
 
-#define SNAKE(x,y) fill_cell(surface,x,y,COLOR_WHITE)  // for snake
-#define DRAW_GRID draw_grid(surface) // for the grid
+#define SNAKE(x,y) fill_cell(psurface,x,y,COLOR_WHITE)  // for snake
+#define DRAW_GRID draw_grid(psurface) // for the grid
 
-#define APPLE(x,y) fill_cell(surface,x,y,COLOR_APPLE)
+#define APPLE(x,y) fill_cell(psurface,x,y,COLOR_APPLE)
+
+
+
+//  creating data structure to store the snake coord and whole snake body coords like linked list
+struct SnakeElement
+{
+    int x, y;
+
+    //  can be null in case of last element
+    struct SnakeElement *pnext;
+};
+
 //  funciton for the drawing grid
-int draw_grid(SDL_Surface *surface){
+int draw_grid(SDL_Surface *psurface){
     //  making horizontal line for the gird 
     SDL_Rect line = {0, 0, WIDTH, LINE_WIDTH};
     for (line.y = 0; line.y < HEIGHT;line.y+=CELL_SIZE){ // line is moved down at every step by cell size 
         //  each iteration draw one horizontal line
-        SDL_FillSurfaceRect(surface, &line, COLOR_GRID);
+        SDL_FillSurfaceRect(psurface, &line, COLOR_GRID);
     }
 
     //  making vertical line for the grid
@@ -31,15 +43,25 @@ int draw_grid(SDL_Surface *surface){
 
     for (line.x = 0; line.x < WIDTH;line.x+=CELL_SIZE)
     {
-        SDL_FillSurfaceRect(surface, &line, COLOR_GRID);
+        SDL_FillSurfaceRect(psurface, &line, COLOR_GRID);
     }
 }
 
 
 //  for snake 
-void fill_cell(SDL_Surface* surface, int x,int y,Uint32 color){
+void fill_cell(SDL_Surface* psurface, int x,int y,Uint32 color){
     SDL_Rect rect = {x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE};
-    SDL_FillSurfaceRect(surface, &rect, color);
+    SDL_FillSurfaceRect(psurface, &rect, color);
+}
+
+//  funciton to draw snake
+void draw_snake(SDL_Surface *psurface,struct SnakeElement *psnake){
+    if(psnake!=NULL){
+
+        SNAKE(psnake->x, psnake->y);
+        draw_snake(psurface, psnake->pnext);
+    }
+
 }
 
 int main(){
@@ -48,13 +70,18 @@ int main(){
     SDL_Init(SDL_INIT_VIDEO); // initialization for graphic lib
     SDL_Window* window= SDL_CreateWindow("Classic Snake", WIDTH, HEIGHT,0); // creating an window which opens in center of screen
 
-    SDL_Surface *surface = SDL_GetWindowSurface(window); //drawing on window
+    SDL_Surface *psurface = SDL_GetWindowSurface(window); //drawing on window
 
     SDL_Event event;
+
+    struct SnakeElement snake = {8,5,NULL};
+    {
+        /* data */
+    };
+    
     //  writing game loop
     int game = 1;
-    int snake_x = 5;
-    int snake_y = 5;
+  
     int apple_x = 10;
     int apple_y = 14;
     while(game){
@@ -65,27 +92,30 @@ int main(){
             }
             if(event.type==SDL_EVENT_KEY_DOWN){
                 if(event.key.key==SDLK_RIGHT){
-                    snake_x++;
+                    
+                    snake.x++;
                 }
                 if(event.key.key==SDLK_LEFT){
-                    snake_x--;
+                    snake.x--;
                 }
                 if(event.key.key==SDLK_UP){
-                    snake_y--;
+                    snake.y--;
                 }
                 if(event.key.key==SDLK_DOWN){
-                    snake_y++;
+                    snake.y++;
+                    
                 }
             }
         }
+
         
+        draw_snake(psurface, &snake);
         
-       
-        SNAKE( snake_x, snake_y);
         APPLE(apple_x, apple_y);
         DRAW_GRID;
         SDL_UpdateWindowSurface(window);
         SDL_Delay(20);
     }
 
+    
 }
